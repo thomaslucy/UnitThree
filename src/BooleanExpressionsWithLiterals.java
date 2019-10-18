f96e6c788710e5270a61a65a6504a255c5c18c68
thomalu <thomalu@ssfs.org>
2019-10-18 11:00:17 -0400

    Finished BooleanExpressionsWithLiterals

---
+++ src/BooleanExpressionsWithLiterals.java
@@ -0,0 +1,17 @@
+public class BooleanExpressionsWithLiterals
+{
+    public static void main(String [] args)
+    {
+        System.out.println(5*18 != 5*3+5*6);
+        System.out.println(-13 == -13.0);
+        System.out.println(-13 <= -13.0);
+        System.out.println(15 % 5 == 0);
+        System.out.println(7 + 9 / 2 >= 7 + 9 % 2);
+        System.out.println(" ");
+        System.out.println(7.1 + 9.5 != 16.6);
+        System.out.println(5.2 < 6.1);
+        System.out.println(100 % 2 == 0);
+        System.out.println(0 % 2 == 0);
+        System.out.println(100 % 3 == 0);
+    }
+}
