237ed41df314cba7d55d4d1208299025f376d534
thomalu <thomalu@ssfs.org>
2019-10-28 10:15:21 -0400

    working on ds and rs

---
+++ src/DiscriminantAndRoots.java
@@ -0,0 +1,9 @@
+public class DiscriminantAndRoots {
+    public static String DARmethod(int a, int b, int c) {
+        if(Math.pow(b,2)-4*a*c>0))
+        return "2 imaginary";
+if (Math.pow())
+
+
+    }
+}
