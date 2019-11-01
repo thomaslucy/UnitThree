88f78d4de8d2c4e18ad7696e0785496028744ac7
thomalu <thomalu@ssfs.org>
2019-11-01 15:09:54 -0400

    Working on cookieorders

---
+++ src/CookieOrders.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CookieOrders
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of boxes you wish to purchase: ");
+        int box = scan.nextInt();
+    }
+    public static String TotalCost(int numboxes)
+    {
+        if (numboxes>0){
+            double cost = 6.95;
+            double total = cost*numboxes
+        }
+    }
+}
