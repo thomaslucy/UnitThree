d2ac9bfc27a61f827e5b289a610b8ef099ea73b6
Lucy <thomalu@ssfss.org>
2019-11-07 16:51:42 -0500

    finished RockPaperScissors

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,9 +1,9 @@
+/* Lucy Thomas
+11/7/2019
+Playing rock paper scissors against the computer using multiple methods */
 import java.util.Scanner;
 
 public class RockPaperScissors
-// Lucy Thomas
-// 11/7/2019
-// Playing rock paper scissors against the computer using multiple methods
 {
     public static String getUserChoice(){
         Scanner scan = new Scanner(System.in);
@@ -15,11 +15,32 @@ public class RockPaperScissors
         int num = (int)(Math.random()*3+1);
         if (num == 1)
             return "rock";
-        if (num == 2)
+        else if (num == 2)
             return "paper";
         else
             return "scissors";
     }
-    
+    public static String whoWins(String computer, String person){
+        if (computer.equals(person))
+            return "You chose "+person+".\nThe computer chose "+computer+".\n You tied!";
+        else if(person.equals("rock") && computer.equals("paper"))
+            return "You chose "+person+".\nThe computer chose "+computer+".\nThe computer wins!";
+        else if(person.equals("paper") && computer.equals("rock"))
+            return "You chose "+person+".\nThe computer chose "+computer+".\nYou win!";
+        else if(person.equals("scissors") && computer.equals("rock"))
+            return "You chose "+person+".\nThe computer chose "+computer+".\nThe computer wins!";
+        else if(person.equals("rock") && computer.equals("scissors"))
+            return "You chose "+person+".\nThe computer chose "+computer+".\nYou win!";
+        else if(person.equals("paper") && computer.equals("scissors"))
+            return "You chose "+person+".\nThe computer chose "+computer+".\nThe computer wins!";
+        else
+            return "You chose "+person+".\nThe computer chose "+computer+".\nYou win!";
+    }
+public static void main(String [] args){
+        String person = getUserChoice();
+        String computer = getComputerChoice();
+        System.out.println(whoWins(computer, person));
+}
+
 
 }
